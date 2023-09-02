f59910584e2778b67b1220fb71cfa4936e2d30d5
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2023-09-02 22:01:42 +0530

    malloc_with_check now prints the errno msg

--- src/utils/mem.c
+++ src/utils/mem.c
@@ -1,10 +1,12 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void *malloc_with_check(size_t size) {
     void *p = malloc(size);
     if (p == NULL) {
-        fprintf(stderr, "Could not allocate memory of size: %zu\n", size);
+        fprintf(stderr, "Could not allocate memory of size: %zu\n: %s\n", size, strerror(errno));
         exit(1);
     }
     return p;
