198b3205e5d1e382cdb201c30a4192a8a614a811
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2023-09-29 12:38:50 +0530

    csv: parser CSV file into a Mat

--- src/parsers/csv.c
+++ src/parsers/csv.c
@@ -1,5 +1,8 @@
 #include "parsers/csv.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "ds/mat.h"
 #include "utils/mem.h"
 
@@ -31,4 +34,37 @@ void csv_print(CSV *csv) {
     }
 
     mat_print(csv->values);
+}
+
+CSV *csv_parse(CSV *csv, const char *filename) {
+    // the parsing is done using scansets
+
+    const int MAX_SCANSET = 4096;
+    const int MAX_ITEM_BUF = 128;
+
+    char scanset[MAX_SCANSET];
+
+    // a scanset that parses until the delimiter and also read the delimiter at the end
+    snprintf(scanset, MAX_SCANSET, "%%[^%c\n]%%c", csv->delim);
+
+    FILE *fp = fopen(filename, "r");
+
+    char item[MAX_ITEM_BUF];
+    char curr_delim;
+
+    size_t r = 0;
+    size_t c = 0;
+
+    while (fscanf(fp, scanset, item, &curr_delim) != EOF) {
+        MAT_AT(csv->values, r, c) = strtof(item, NULL);
+        c++;
+
+        if (curr_delim == '\n') {
+            r++;
+            c = 0;
+        }
+    }
+
+    fclose(fp);
+    return csv;
 }
