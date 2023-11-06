5930a05d852601f5185dd05aba4e839656b34d20
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2023-11-06 23:17:14 +0530

    metrics: defined distance functions

--- include/model/metrics_pairwise.h
+++ include/model/metrics_pairwise.h
@@ -15,3 +15,32 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
+
+#ifndef MODEL_METRICS_PAIRWISE_H
+#define MODEL_METRICS_PAIRWISE_H
+
+#include <gsl/gsl_vector.h>
+
+#include "ds/array.h"
+
+typedef enum {
+    cityblock = 1,
+    cosine,
+    euclidean,
+    haversian,
+    l1,
+    l2,
+    manhattan
+} distance_metric_type;
+
+/*
+Computes the distance between two points provided as gsl_vectors
+*/
+double two_point_distance_gsl(gsl_vector *p1, gsl_vector *p2, distance_metric_type type);
+
+/*
+Computes the distance between two points provided as an Array
+*/
+double two_point_distance_arr(Array *p1, Array *p2, distance_metric_type type);
+
+#endif
